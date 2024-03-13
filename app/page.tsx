b07000b414359bf066fd0489166ab9a16e471ee3
ZzMEGAzZ <apisitsangkrajang@gmail.com>
2024-03-13 22:20:20 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
-import { Button } from '@/components/interactives/Button';
 import Middle from '@/components/layouts/Middle';
-import SideNav from '@/components/objects/sideNavigation/SideNavigation';
 import React, { useEffect } from 'react';
-import toast, { Toaster } from 'react-hot-toast';
 
 export default function Home() {
     return (
